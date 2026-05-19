d148b3c778b02135bd906310f8b0c47bf806e8a1
Murilo <57834900+muriloonunes@users.noreply.github.com>
2026-05-19 11:38:25 -0300

    feat(categorias): implementa a lista de categorias com funcionalidade de criação

---
+++ frontend/src/app/services/categoria-service/categorias-service.ts
@@ -0,0 +1,24 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CategoriaModel} from '../../models/CategoriaModel';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriasService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/categorias';
+
+  obterCategorias(): Observable<CategoriaModel[]> {
+    return this.http.get<CategoriaModel[]>(`${this.apiUrl}`);
+  }
+
+  criarCategoria(categoria: CategoriaModel): Observable<CategoriaModel> {
+    return this.http.post<CategoriaModel>(`${this.apiUrl}`, categoria);
+  }
+
+  apagarCategoria(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
