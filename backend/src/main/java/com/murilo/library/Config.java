2ac2c23b2ed6ea237a5d312f1ef3e3287a908706
Murilo <57834900+muriloonunes@users.noreply.github.com>
2026-05-19 12:34:26 -0300

    feat(backend): adiciona o CORS pros requests do navegador e melhora o tratamento de excecoes pro backend

---
+++ backend/src/main/java/com/murilo/library/Config.java
@@ -0,0 +1,22 @@
+package com.murilo.library;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Murilo Nunes <murilo_no@outlook.com>
+ * @date 19/05/2026
+ * @brief Class Config
+ */
+@Configuration
+public class Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
