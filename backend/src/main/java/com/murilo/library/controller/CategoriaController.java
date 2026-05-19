94d883eade04644bb1a1ec2ef04a37c310d2e45e
murilo <57834900+muriloonunes@users.noreply.github.com>
2026-05-18 22:16:20 -0300

    feat(categoria): implementa o CategoriaController

---
+++ backend/src/main/java/com/murilo/library/controller/CategoriaController.java
@@ -0,0 +1,48 @@
+package com.murilo.library.controller;
+
+import com.murilo.library.entities.Categoria;
+import com.murilo.library.entities.dto.CategoriaDTO;
+import com.murilo.library.service.CategoriaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ *
+ * @author Murilo Nunes <murilo_no@outlook.com>
+ * @date 18/05/2026
+ * @brief Class CategoriaController
+ */
+@RequestMapping("/categorias")
+@RestController
+public class CategoriaController {
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody CategoriaDTO categoriaDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.criarCategoria(categoriaDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listar() {
+        return ResponseEntity.ok(categoriaService.listarCategorias());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(categoriaService.encontrarPorId(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        categoriaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
